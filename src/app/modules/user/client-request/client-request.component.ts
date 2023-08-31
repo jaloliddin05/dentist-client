4001bdae150e73f404e25d60f522f1fc30d5d2b2
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-08-31 14:10:10 +0500

    ...

--- src/app/modules/user/client-request/client-request.component.ts
+++ src/app/modules/user/client-request/client-request.component.ts
@@ -20,4 +20,6 @@ export class ClientRequestComponent {
       time: ['', Validators.required],
     });
   }
+
+  onSubmit() {}
 }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ClientRequestComponent } from './client-request/client-request.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserProfileComponent, ClientRequestComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class UserModule {}
